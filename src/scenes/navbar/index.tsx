c9e65f8d77099bbe4fd5dc0f8a1ee73f0c9d8b86
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-20 21:06:12 -0500

    sidebar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -61,20 +61,56 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                                 </div>
                             </div>
                         ) : (
-                            <div>
-                                <button
-                                    className="bg-secondary-500 rounded-full p-2"
-                                    onClick={() =>
-                                        setIsMenuToggleed(!isMenuToggled)
-                                    }
-                                >
-                                    <Bars3Icon className="h-6 w-6 text-white" />
-                                </button>
-                            </div>
+                            <button
+                                className="bg-secondary-500 rounded-full p-2"
+                                onClick={() =>
+                                    setIsMenuToggleed(!isMenuToggled)
+                                }
+                            >
+                                <Bars3Icon className="h-6 w-6 text-white" />
+                            </button>
                         )}
                     </div>
                 </div>
             </div>
+
+            {/* Mobile Menu Modal */}
+            {!isAboveMediumScreens && isMenuToggled && (
+                <div className="bg-primary-100 fixed bottom-0 right-0 z-40 h-full w-[300px] drop-shadow-xl">
+                    {/* Close Icon */}
+                    <div className="flex justify-end p-12">
+                        <button
+                            onClick={() => setIsMenuToggleed(!isMenuToggled)}
+                        >
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+
+                    {/* Menu Items */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                        <Link
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                            page="Home"
+                        />
+                        <Link
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                            page="Benefits"
+                        />
+                        <Link
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                            page="Our Classes"
+                        />
+                        <Link
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                            page="Contact Us"
+                        />
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
-                'gray-20': '#f7f2fa',
-                'gray-50': '#e9dfeb',
-                'gray-100': '#ddcfde',
-                'gray-500': '#5a0214',
-                'primary-100': '#ffe3e9',
-                'primary-300': '#ffa0aa',
-                'primary-500': '#ff6c73',
-                'secondary-400': '#ff82db',
-                'secondary-500': '#ff4bd5',
+                "gray-20": "#f7f2fa",
+                "gray-50": "#e9dfeb",
+                "gray-100": "#ddcfde",
+                "gray-500": "#5a0214",
+                "primary-100": "#ffe3e9",
+                "primary-300": "#ffa0aa",
+                "primary-500": "#ff5853",
+                "secondary-400": "#ff82db",
+                "secondary-500": "#ff4bd5",
             },
             backgroundImage: (theme) => ({
-                'gradient-yellowred':
-                    'linear-gradient(90deg, #ff7064 0%, #f2d028 100%)',
-                'mobile-home': "url('./assets/HomePageGraphic.png')",
+                "gradient-yellowred":
+                    "linear-gradient(90deg, #ff7064 0%, #f2d028 100%)",
+                "mobile-home": "url('./assets/HomePageGraphic.png')",
             }),
             fontFamily: {
-                dmsans: ['DM Sans', 'sans-serif'],
-                montserrat: ['Montserrat', 'sans-serif'],
+                dmsans: ["DM Sans", "sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
             },
             content: {
                 evolvetext: "url('./assets/EvolveText.png')",
@@ -31,10 +31,10 @@ export default {
             },
         },
         screens: {
-            xs: '480px',
-            sm: '768px',
-            md: '1060',
+            xs: "480px",
+            sm: "768px",
+            md: "1060",
         },
     },
-    plugins: ['prettier-plugin-tailwindcss'],
-}
+    plugins: ["prettier-plugin-tailwindcss"],
+};
